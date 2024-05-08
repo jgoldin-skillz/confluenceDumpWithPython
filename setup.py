d82f6f0738af2ccb00f5e47c3ae6c7e075ded349
jgoldin-skillz <60894521+jgoldin-skillz@users.noreply.github.com>
2024-05-08 10:37:30 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='confluence_dump',
+    version='0.0.1',
+    url='https://github.com/jgoldin-skillz/confluenceDumpWithPython.git',
+    packages=find_packages(),    
+    install_requires=['beautifulsoup4', 'Pillow', 'pandoc', 'pypandoc', 'requests'],
+)
