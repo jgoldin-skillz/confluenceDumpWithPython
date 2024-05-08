b9112ed5cd3ba0485a3446a31c556aff529e2695
jgoldin-skillz <60894521+jgoldin-skillz@users.noreply.github.com>
2024-05-08 10:44:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='confluence_dump',
     version='0.0.1',
     url='https://github.com/jgoldin-skillz/confluenceDumpWithPython.git',
-    py_modules=['confluence_dump'],
+    packages=['confluence_dump'],
     install_requires=['beautifulsoup4', 'Pillow', 'pandoc', 'pypandoc', 'requests'],
 )
